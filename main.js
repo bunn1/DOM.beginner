5f542b81f6a4265628f689f8262fd49e580dd956
bunn1 <nicjohbun@gmail.com>
2022-07-11 08:29:24 +0200

    toggle line code

--- main.js
+++ main.js
@@ -2,18 +2,22 @@
 
 
 // GetElementById()
-const title = document.getElementById("main-heading");
-console.log(title);
+// const title = document.getElementById("main-heading");
+// console.log(title);
 
 // GelElementByClassName()
 
-const listItem1 = document.getElementsByClassName("list-items");
-console.log(listItem1)
+// const listItem1 = document.getElementsByClassName("list-items");
+// console.log(listItem1)
 
 // GetElementsByTagName()
-const listItems = document.getElementsByTagName("li");
-console.log(listItems)
+// const listItems = document.getElementsByTagName("li");
+// console.log(listItems)
 
 // querySelector
-const container = document.querySelector("div");
-console.log(container)
+// const container = document.querySelector("div");
+// console.log(container)
+
+// querySelectorAll
+// const container1= document.querySelectorAll("div");
+// console.log(container1)
