e91815938df04b619b4a4c92c4dc1e9423a47517
bunn1 <nicjohbun@gmail.com>
2022-07-11 17:33:50 +0200

    new container, eventListener

--- main2.js
+++ main2.js
@@ -1,1 +1,29 @@
 // Event Listener
+
+// element.addEventListener("click", function();
+
+const buttonTwo = document.querySelector('#btn-2');
+
+buttonTwo.addEventListener('click', function (){
+
+alert('I love Css');
+
+});
+
+// MouseOver
+
+const newBackgroundColor = document.querySelector('.box-3');
+
+function changeBgColor(){
+    newBackgroundColor.style.backgroundColor ="red";
+}
+newBackgroundColor.addEventListener('mouseover', changeBgColor); 
+
+
+
+
+
+
+
+
+
