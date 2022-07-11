5b1bd64f4a987dd426b919389e8e301f398cf1e6
bunn1 <nicjohbun@gmail.com>
2022-07-11 18:44:33 +0200

    reveal or hide content function js

--- main2.js
+++ main2.js
@@ -17,8 +17,22 @@ const newBackgroundColor = document.querySelector('.box-3');
 function changeBgColor(){
     newBackgroundColor.style.backgroundColor ="red";
 }
-newBackgroundColor.addEventListener('mouseover', changeBgColor); 
+newBackgroundColor.addEventListener('mouseover', changeBgColor);
 
+// Hide or Reveal Content
+
+const revealBtn = document.querySelector(".reveal-button");
+const hiddenContent = document.querySelector(".hidden-content");
+
+function revealContent() {
+
+    if (hiddenContent.classList.contains(".reveal-button")
+){ 
+    hiddenContent.classList.remove(".reveal-button")
+     }else{
+        hiddenContent.classList.add(".reveal-button")
+ }
+}
 
 
 
