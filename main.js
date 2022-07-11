9e1ac742c3d46b8f0e863285ddab737dfb8ea728
bunn1 <nicjohbun@gmail.com>
2022-07-11 13:43:49 +0200

    console.log, new file main2.js

--- main.js
+++ main.js
@@ -63,9 +63,39 @@ console.log(li.classList.contains("list-items"));
 // Remove Elements --------------------------------------------
 // li.remove();
 
-// Parent Node  --------------------------------
-let ulList = document.querySelector("ul")
-console.log(ul.parentNode);
-console.log(ul.parentElement);
+// Seek for Parent Node  --------------------------------
+// let ulList = document.querySelector("ul")
+// console.log(ul.parentNode);
+// console.log(ul.parentElement);
+// Seek for Grand Parent
+// console.log(ul.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement);
 
-// 
+// const html = document.documentElement;
+// console.log(html)
+
+// Seek for Child Node ------------------------------------
+// let ulList = document.querySelector("ul")
+
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+
+// To be able to style first child node - [number] ------------------------
+// ul.childNodes[1].style.backgroundColor = "blue";
+
+// console.log(ul.children);
+// console.log(ul.firstElementChild);
+// console.log(ul.lastElementChild);
+
+// Seek Sibling Node ------------------------
+let ulList = document.querySelector("ul")
+const div = document.querySelector("div");
+
+console.log(div.childNodes)
+
+console.log(ul.previousSibling);
+console.log(ul.nextSibling);
+
+console.log(ul.previousElementSibling);
+console.log(ul.nextElementSibling);
