2c7b34d5d99ce712466b0bf348d9c3204eb7f84b
bunn1 <nicjohbun@gmail.com>
2022-07-12 22:51:34 +0200

    event delegation

--- main2.js
+++ main2.js
@@ -41,62 +41,24 @@ revealBtn.addEventListener('click', function handleClick() {
 
 // Nothing on Probagation
 
+
+
+// document.querySelector("#football").addEventListener("click", function (e) {
+//     console.log("football is clicked");
+
+//     const target = e.target;
+
+//     if (
+//         target.matches("li")) {
+//         target.style.backgroundColor = "lightgrey";
+//     }
+// })
+
+
 // Event Delegation
 
-document.querySelector("#football").addEventListener("click", function (e) {
-    console.log("football is clicked");
+document.querySelector("#sports").addEventListener("click", function (e) {
+    console.log(e.target.getAttribute("id") + "is clicked");
 
-    const target = e.target;
-
-    if (
-        target.matches("li")) {
-        target.style.backgroundColor = "lightgrey";
-    }
 })
 
-
-document.querySelector("#basketball").addEventListener("click", function (e) {
-    console.log("basketball is clicked");
-
-    const target = e.target;
-
-    if (
-        target.matches("li")) {
-        target.style.backgroundColor = "lightgrey";
-    }
-})
-
-
-document.querySelector("#boxing").addEventListener("click", function (e) {
-    console.log("boxing is clicked");
-
-    const target = e.target;
-
-    if (
-        target.matches("li")) {
-        target.style.backgroundColor = "lightgrey";
-    }
-})
-
-
-document.querySelector("#tennis").addEventListener("click", function (e) {
-    console.log("tennis is clicked");
-
-    const target = e.target;
-
-    if (
-        target.matches("li")) {
-        target.style.backgroundColor = "lightgrey";
-    }
-})
-
-document.querySelector("#golf").addEventListener("click", function (e) {
-    console.log("golf is clicked");
-
-    const target = e.target;
-
-    if (
-        target.matches("li")) {
-        target.style.backgroundColor = "lightgrey";
-    }
-})
