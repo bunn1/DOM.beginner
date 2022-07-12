1a91274589080ae7d14225a25c2f23326bf61cb1
bunn1 <nicjohbun@gmail.com>
2022-07-12 22:22:31 +0200

    copy addeventlistener

--- main2.js
+++ main2.js
@@ -53,3 +53,50 @@ document.querySelector("#football").addEventListener("click", function (e) {
         target.style.backgroundColor = "lightgrey";
     }
 })
+
+
+document.querySelector("#basketball").addEventListener("click", function (e) {
+    console.log("basketball is clicked");
+
+    const target = e.target;
+
+    if (
+        target.matches("li")) {
+        target.style.backgroundColor = "lightgrey";
+    }
+})
+
+
+document.querySelector("#boxing").addEventListener("click", function (e) {
+    console.log("boxing is clicked");
+
+    const target = e.target;
+
+    if (
+        target.matches("li")) {
+        target.style.backgroundColor = "lightgrey";
+    }
+})
+
+
+document.querySelector("#tennis").addEventListener("click", function (e) {
+    console.log("tennis is clicked");
+
+    const target = e.target;
+
+    if (
+        target.matches("li")) {
+        target.style.backgroundColor = "lightgrey";
+    }
+})
+
+document.querySelector("#golf").addEventListener("click", function (e) {
+    console.log("golf is clicked");
+
+    const target = e.target;
+
+    if (
+        target.matches("li")) {
+        target.style.backgroundColor = "lightgrey";
+    }
+})
