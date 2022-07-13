ae55654ee3d9e99804dc7910b97e2f447c9d265e
bunn1 <nicjohbun@gmail.com>
2022-07-13 21:06:45 +0200

    array of quotes

---
+++ quoteGeneratorJs/main.js
@@ -0,0 +1,33 @@
+// Variables
+
+let btn = document.querySelector("#new-quote");
+let quote = document.querySelector(".quote");
+let person = document.querySelector(".person");
+
+
+const quotes = [{
+
+    quote: `"Nothing in life is to be feared, it is only to be understood. 
+    Now is the time to understand more, so that we may fear less."`,
+    person: `Marie Curie`
+},{
+    quote: `"Our prime purpose in this life is to help others. 
+    And if you can't help them, at least don't hurt them."`,
+    person: `Dalai Lama`
+
+},{
+    quote: `"If you want to live a happy life, tie it to a goal, not
+     to people or things."`,
+    person: `Albert Einstein`
+
+},{
+    quote: `"Life is what happens while you are busy making other plans."`,
+    person: `Eminem`
+
+},{
+    quote: `"Life's most persistent and urgent question
+     is, 'What are you doing for others?'"`,
+    person: `Martin Luther King Jr`
+
+
+}]
