29d2427c0dbcab8862af237b30cd5c926c45afad
bunn1 <nicjohbun@gmail.com>
2022-07-14 13:14:51 +0200

    array, Math floor

---
+++ quoteGeneratorTwo/main.js
@@ -0,0 +1,10 @@
+let quotes =[
+    'Life is what happens to us while we are making other plans."- Allen Saunders',
+    'Life isn’t about finding yourself. Life is about creating yourself.”― George Bernard Shaw',
+    'Doubt kills more dreams than failure ever will.” – Suzy Kassem',
+    'Keep your face always toward the sunshine, and shadows will fall behind you.” – Walt Whitman',
+    'The happiness of your life depends on the quality of your thoughts.” – Marcus Aurelius'
+]
+
+let random = Math.floor(Math.random()) * quotes.length;
+console.log(random)
