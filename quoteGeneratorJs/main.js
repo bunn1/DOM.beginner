378ca4c22f2b78bdb774d4c5f4365d9f7c3d8f0e
bunn1 <nicjohbun@gmail.com>
2022-07-14 12:05:54 +0200

    corrections

--- quoteGeneratorJs/main.js
+++ quoteGeneratorJs/main.js
@@ -9,43 +9,36 @@ const quotes = [{
     quote: `"Nothing in life is to be feared, it is only to be understood. 
     Now is the time to understand more, so that we may fear less."`,
     person: `Marie Curie`
-},{
+}, {
     quote: `"Our prime purpose in this life is to help others. 
     And if you can't help them, at least don't hurt them."`,
     person: `Dalai Lama`
 
-},{
+}, {
     quote: `"If you want to live a happy life, tie it to a goal, not
      to people or things."`,
     person: `Albert Einstein`
 
-},{
+}, {
     quote: `"Life is what happens while you are busy making other plans."`,
     person: `Eminem`
 
-},{
+}, {
     quote: `"Life's most persistent and urgent question
      is, 'What are you doing for others?'"`,
     person: `Martin Luther King Jr`
 
-},{
+}, {
     quote: `"Remember that not getting what you want is sometimes a wonderful stroke of luck."`,
     person: `Dalai Lama`
 }]
 
+btn.addEventListener('click', function () {
 
-// btn.addEventListener('click', function(){
+    let random = [Math.floor(Math.random() * quotes.length)]
 
-//     let random = Math.floor(Math.random() * quotes.length);
-         
-// })
+    quote.innerText = quotes[random].quote;
+    person.innerText = quotes[random].person;
 
-btn.addEventListener('click', function(){
-    
-    let random =[Math.floor(Math.random() * quotes.length)]
 
-  quote.innerText = quotes[random].quote;
-  person.innerText = quotes[random].person;
 })
-
-
