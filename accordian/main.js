dd069bbc98911c997e47257511f7580ef05ff1d5
bunn1 <nicjohbun@gmail.com>
2022-07-15 09:36:53 +0200

    corrections

--- accordian/main.js
+++ accordian/main.js
@@ -1,11 +1,10 @@
 // Variable
 
-const accordian = document.getQuerySelector('#content-container');
+const accordian = document.getElementsByClassName('.content-container');
 
 // Access info in container - for loop
 for ( i = 0; i < accordian.length; i++) {
 
-    
 //   Reference point all content-container / toggle = toggle 
 //   between adding or removing a classname with the method active
     accordian[i].addEventListener('click', function() {
