b593a1e2e16432d5feb3a7fa258c566f678007da
bunn1 <nicjohbun@gmail.com>
2022-07-16 13:55:42 +0200

    corrections

--- accordian/main.js
+++ accordian/main.js
@@ -6,8 +6,10 @@ let accordian = document.getElementsByClassName('.content-container');
 for ( i = 0; i < accordian.length; i++) {
 
 //   Reference point all content-container / toggle = toggle 
-//   between adding or removing a classname with the method active
+//   adding or removing a classname with the method active
     accordian[i].addEventListener('click', function() {
         this.classList.toggle("active");
     })
 }
+
+console.log()
