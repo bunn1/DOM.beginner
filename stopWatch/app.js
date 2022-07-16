430a7ea1fe354e02d10f77a9789c236cc7e669fb
bunn1 <nicjohbun@gmail.com>
2022-07-16 13:05:22 +0200

    function sec,min,hours

--- stopWatch/app.js
+++ stopWatch/app.js
@@ -2,3 +2,22 @@
 
 const startStopBtn = document.querySelector("#startStopBtn");
 const resetBtn = document.querySelector("#resetBtn");
+
+// Variables for time values
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+
+// Stop Watch Function
+
+function stopWatch() {
+
+    seconds ++;
+
+    if (seconds / 60 === 1){
+        seconds = 0
+        minutes ++;
+    }
+}
