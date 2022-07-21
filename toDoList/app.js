9cbeed641eae3e8c5adad037d6f2569f29a98a5d
bunn1 <nicjohbun@gmail.com>
2022-07-21 23:30:43 +0200

    delete

--- toDoList/app.js
+++ toDoList/app.js
@@ -7,44 +7,29 @@ const deleteBtn = document.getElementById("delete-btn");
 
 
 
-// addTask.addEventListener("click", function (){
-
-//     // Create div element
-//     let task = document.createElement("div");
-//     // Add a class to the task so we can style it
-//     task.classList.add("task");
-
-//     // Create list element
-//     let li = document.createElement("li");
-//     li.innerText = `$`
-
-// })
-
-
-function fun1(){
-    taskContainer.innerHTML = inputTask.value 
+function fun1() {
+    taskContainer.innerHTML = inputTask.value
 }
 addTask.addEventListener("click", fun1);
-   
-function clearContainerField(){
+
+function clearContainerField() {
 
     const el = document.getElementById("task-container");
 
-    for (i = 0; i < el.inputTask.value; i++){
+    for (i = 0; i < el.inputTask.value; i++) {
 
     }
 }
 
-taskContainer.addEventListener("click", ()=> {
+taskContainer.addEventListener("click", () => {
+    taskContainer.style.textDecoration = "line-through";
+    taskContainer.style.textDecorationThickness = "4px";
+    taskContainer.style.textDecorationColor = "green";
 
-taskContainer.style.textDecoration = "line-through";
-taskContainer.style.textDecorationThickness = "4px";
-taskContainer.style.textDecorationColor = "green";
 
+    deleteBtn.addEventListener("click", (e) => {
+        deleteBtn.remove(e);
+        taskContainer.remove(e);
 
-deleteBtn.addEventListener("click",(e) => {
-    deleteBtn.remove(e);
-    taskContainer.remove(e);
-
+    })
 })
- })
