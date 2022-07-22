ee85c76e4e8d9cbfac6647f41231131f5b38dbd9
bunn1 <nicjohbun@gmail.com>
2022-07-22 19:11:34 +0200

    js-variables, html-divs-ul-button

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,36 +1,7 @@
-// Variables
-
-const addTask = document.getElementById("add-task");
-const taskContainer = document.getElementById("task-container");
-const inputTask = document.getElementById("input-task");
-const deleteBtn = document.getElementById("delete-btn");
+let inp = document.getElementById("inp"); // Input field
+let but = document.getElementById("but"); // Button Add
+let list = document.getElementById("list"); //Getting value of unordered List
 
 
+but.addEventListener("click", addToDo) // Calls the addToDo() function by click on the Add Btn
 
-function fun1() {
-    taskContainer.innerHTML = inputTask.value
-}
-addTask.addEventListener("click", fun1);
-
-function clearContainerField() {
-
-    const el = document.getElementById("task-container");
-
-    for (i = 0; i < el.inputTask.value; i++) {
-
-    }
-}
-
-taskContainer.addEventListener("click", () => {
-    taskContainer.style.textDecoration = "line-through";
-    taskContainer.style.textDecorationThickness = "4px";
-    taskContainer.style.textDecorationColor = "orange";
-
-
-    deleteBtn.addEventListener("click", (value) => {
-    
-        taskContainer.remove(value);
-        value.removChild(value);
-
-    })
-})
